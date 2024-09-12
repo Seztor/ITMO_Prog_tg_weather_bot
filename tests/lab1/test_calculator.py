3bec9c39889a86feee5f4ada3bf8a2ce54c6dbd9
Pavel <pavel.govorov.06@mail.ru>
2024-09-12 19:47:09 +0300

    [feat]: added func for calc

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,11 @@
 import unittest
+from src.lab1.calculator import calc
 
 class CalculatorTestCase(unittest.TestCase):
 
     # Тест для проверки работы, можно удалить
     def test_one(self):
-        self.assertEquals(1, 1)
+        self.assertEqual(1, 1)
+
+    def test_addition(self):
+        self.assertEqual(calc(1,2),3)
